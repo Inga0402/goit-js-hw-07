fa298b553d43fcab089894aea6edf2bc876f9dba
Inga Grigorieva <ingagrigorieva0402@gmail.com>
2020-12-16 18:47:13 +0200

    исправлена ошибка в task 2

--- js/task-2.js
+++ js/task-2.js
@@ -7,26 +7,37 @@ const ingredients = [
   'Приправы',
 ];
 
-const listRef = document.querySelector('ul#ingredients');
+const listRef = document.querySelector('ul');
 
-const elementRef1 = document.createElement('li');
-elementRef1.textContent = ingredients[0];
+ingredients.forEach(element => {
+  const elementRef = document.createElement('li');
+  elementRef.textContent = element;
 
-const elementRef2 = document.createElement('li');
-elementRef2.textContent = ingredients[1];
+  listRef.append(elementRef);
+  console.log(elementRef);
 
-const elementRef3 = document.createElement('li');
-elementRef3.textContent = ingredients[2];
+} );
 
-const elementRef4 = document.createElement('li');
-elementRef4.textContent = ingredients[3];
+// const listRef = document.querySelector('ul#ingredients');
 
-const elementRef5 = document.createElement('li');
-elementRef5.textContent = ingredients[4];
+// const elementRef1 = document.createElement('li');
+// elementRef1.textContent = ingredients[0];
 
-const elementRef6 = document.createElement('li');
-elementRef6.textContent = ingredients[5];
+// const elementRef2 = document.createElement('li');
+// elementRef2.textContent = ingredients[1];
 
-listRef.append(elementRef1, elementRef2, elementRef3, elementRef4, elementRef5, elementRef6);
-console.log(listRef);
+// const elementRef3 = document.createElement('li');
+// elementRef3.textContent = ingredients[2];
+
+// const elementRef4 = document.createElement('li');
+// elementRef4.textContent = ingredients[3];
+
+// const elementRef5 = document.createElement('li');
+// elementRef5.textContent = ingredients[4];
+
+// const elementRef6 = document.createElement('li');
+// elementRef6.textContent = ingredients[5];
+
+// listRef.append(elementRef1, elementRef2, elementRef3, elementRef4, elementRef5, elementRef6);
+// console.log(listRef);
 
